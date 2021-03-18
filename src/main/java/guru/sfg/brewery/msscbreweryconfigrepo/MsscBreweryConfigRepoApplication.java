633031bf5160833abf0dc631c26aa1eabfdcdee3
mak199 <mak.khan199@googlemail.com>
2021-03-18 14:52:19 +0100

    initial commit

---
+++ src/main/java/guru/sfg/brewery/msscbreweryconfigrepo/MsscBreweryConfigRepoApplication.java
@@ -0,0 +1,13 @@
+package guru.sfg.brewery.msscbreweryconfigrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryConfigRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBreweryConfigRepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/sfg/brewery/msscbreweryconfigrepo/MsscBreweryConfigRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.sfg.brewery.msscbreweryconfigrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryConfigRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
